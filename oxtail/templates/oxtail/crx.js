ba92ec154c848e433f033ae696756a0f06bdcfae
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-01-31 18:48:11 -0500

    Adding awful hack to make click event get set via an injected script tag with the script encoded as a data URI.

--- oxtail/templates/oxtail/crx.js
+++ oxtail/templates/oxtail/crx.js
@@ -6,7 +6,11 @@ var oxtailInterval = setInterval(function() {
         if (document.getElementById('guser')) {
             clearInterval(oxtailInterval);
             {% include 'oxtail/button_adder.js' %}
-            {% include 'oxtail/loader.js' %}
+            
+            var loader = document.createElement('script');
+            loader.setAttribute('type', 'text/javascript');
+            loader.src = 'data:text/javascript;charset=utf-8,' + encodeURIComponent("{% filter escapejs %}{% include 'oxtail/loader.js' %}{% endfilter %}");
+            document.getElementsByTagName('head')[0].appendChild(loader);
         }
     }
 }, 250);
