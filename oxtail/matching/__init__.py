a84be0b758e2703a46a38ee7b62efc4513ecb654
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2011-02-14 18:02:28 -0500

    Matcher now returns original surface strings of matches.

--- oxtail/matching/__init__.py
+++ oxtail/matching/__init__.py
@@ -4,7 +4,16 @@ from oxtail.matching.matcher import build_token_trie, token_match
 import os
 
 
-_blacklist = set(['us', 'us government'])
+_blacklist = set([
+    'us', 
+    'us government', 
+    'va', 
+    'madison',
+    'alliance',
+    'no one',
+    'imagine',
+    'impact',
+])
 
 
 _entity_trie = build_token_trie(open(os.path.join(os.path.dirname(__file__), 'normalized_aliases.csv'), 'r'), _blacklist)
--- oxtail/matching/matcher.py
+++ oxtail/matching/matcher.py
@@ -2,21 +2,25 @@ from oxtail.matching import basic_normalizer
 from pytrie.pytrie import Trie
 import re
 import csv
+from collections import deque, defaultdict
 
 
 def token_match(trie, text):
-    text = re.split("\W+", basic_normalizer(text))
-    result = list()
+    split_text = re.split("(\W+)", text)
+    remaining_tokens = deque([basic_normalizer(split_text[i]) for i in xrange(0, len(split_text), 2)])
+    result = defaultdict(set)
     
-    i = 0
-    while i < len(text):
-        item = trie.longest_prefix_item(text[i:], default=None)
+    while len(remaining_tokens) > 0:
+        item = trie.longest_prefix_item(remaining_tokens, default=None)
         if item:
-            (match, id) = (" ".join(item[0]), item[1])
-            result.append((match, id))
-            i += len(item[0])
+            match_start = len(split_text) - len(remaining_tokens) * 2 + 1
+            match_end = match_start + len(item[0]) * 2 - 1
+            (match, id) = ("".join(split_text[match_start:match_end]), item[1])
+            result[id].add(match)
+            for _ in range(len(item[0])):
+                remaining_tokens.popleft()
         else:
-            i += 1
+            remaining_tokens.popleft()
     
     return result
 
