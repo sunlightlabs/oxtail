7a9ca6dd991f026fcd72d1152d9f573a8994acad
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-02-15 13:11:32 -0500

    Adding pickle caching of built trie.

--- oxtail/matching/__init__.py
+++ oxtail/matching/__init__.py
@@ -2,6 +2,7 @@ basic_normalizer = str.lower
 
 from oxtail.matching.matcher import build_token_trie, token_match
 import os
+import cPickle
 
 
 _blacklist = set([
@@ -16,7 +17,17 @@ _blacklist = set([
 ])
 
 
-_entity_trie = build_token_trie(open(os.path.join(os.path.dirname(__file__), 'normalized_aliases.csv'), 'r'), _blacklist)
+data_dir = os.path.dirname(__file__)
+trie_file = os.path.join(data_dir, 'normalized_aliases.trie')
+if os.path.exists(trie_file):
+    trie_handle = open(trie_file, 'r')
+    _entity_trie = cPickle.load(trie_handle)
+    trie_handle.close()
+else:
+    _entity_trie = build_token_trie(open(os.path.join(data_dir, 'normalized_aliases.csv'), 'r'), _blacklist)
+    trie_handle = open(trie_file, 'w')
+    cPickle.dump(_entity_trie, trie_handle, cPickle.HIGHEST_PROTOCOL)
+    trie_handle.close()
 
 def match(text):
     return token_match(_entity_trie, text)
