900d014aca1f0151089f0ab8c9c33289ab875003
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2011-05-11 17:28:28 -0400

    [#836] Fixed bug where punctuation wasn't stripped from entity names.

--- oxtail/matching/matcher.py
+++ oxtail/matching/matcher.py
@@ -1,21 +1,28 @@
-from oxtail.matching import basic_normalizer
 from pytrie.pytrie import Trie
 import re
 import csv
 from collections import deque, defaultdict
 
 
+
+def tokenize(text):
+    all_tokens = re.split("(\W+)", text)
+    word_tokens = [all_tokens[i].lower() for i in xrange(0, len(all_tokens), 2)]
+    return word_tokens
+
+
 def token_match(trie, text):
-    split_text = re.split("(\W+)", text)
-    remaining_tokens = deque([basic_normalizer(split_text[i]) for i in xrange(0, len(split_text), 2)])
+    all_tokens = re.split("(\W+)", text)
+    word_tokens = [all_tokens[i].lower() for i in xrange(0, len(all_tokens), 2)]
+    remaining_tokens = deque(word_tokens)
     result = defaultdict(set)
     
     while len(remaining_tokens) > 0:
         item = trie.longest_prefix_item(remaining_tokens, default=None)
         if item:
-            match_start = len(split_text) - len(remaining_tokens) * 2 + 1
+            match_start = len(all_tokens) - len(remaining_tokens) * 2 + 1
             match_end = match_start + len(item[0]) * 2 - 1
-            (match, id) = ("".join(split_text[match_start:match_end]), item[1])
+            (match, id) = ("".join(all_tokens[match_start:match_end]), item[1])
             result[id].add(match)
             for _ in range(len(item[0])):
                 remaining_tokens.popleft()
@@ -26,5 +33,5 @@ def token_match(trie, text):
 
 
 def build_token_trie(norm_file, blacklist={}):
-    return Trie([(l[0].split(), l[1]) for l in csv.reader(norm_file) if l[0] not in blacklist])
+    return Trie([(tokenize(l[0]), l[1]) for l in csv.reader(norm_file) if l[0] not in blacklist])
 
