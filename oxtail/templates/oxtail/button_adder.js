abfa7af8dc53758295a7a4d230202c831d28d080
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-06-06 10:40:40 -0400

    Fixing behavior of HTML button with old browsers.

--- oxtail/templates/oxtail/button_adder.js
+++ oxtail/templates/oxtail/button_adder.js
@@ -1,7 +1,8 @@
 var submit = document.getElementById('oxtail-submit');
 if (!submit) {
     var userArea = document.getElementsByClassName('GcwpPb-MEmzyf')[0].childNodes[0];
-    userArea.innerHTML = userArea.innerHTML + '&nbsp; <span id=\'oxtail-div\' style=\'position: relative\'><button id=\'oxtail-submit\' style=\'background: none repeat scroll 0% 0% transparent; border: 0; padding: 0; margin: 0; width: 166px; height: 34px; position: absolute; left: -166px; top: -28px; cursor: pointer;\'><img id=\'oxtail-submit-img\' src=\'{{ host }}{{ oxtail_media_path }}/img/button-off.png\' style=\'position: absolute; left: 0px; clip: rect(0px,166px,34px,0px);\' /></button></span>';
+    userOffset = userArea.parentNode.id == 'guser' ? 40 : -28;
+    userArea.innerHTML = userArea.innerHTML + '&nbsp; <span id=\'oxtail-div\' style=\'position: relative\'><button id=\'oxtail-submit\' style=\'background: none repeat scroll 0% 0% transparent; border: 0; padding: 0; margin: 0; width: 166px; height: 34px; position: absolute; left: -166px; top: ' + userOffset + 'px; cursor: pointer;\'><img id=\'oxtail-submit-img\' src=\'{{ host }}{{ oxtail_media_path }}/img/button-off.png\' style=\'position: absolute; left: 0px; clip: rect(0px,166px,34px,0px);\' /></button></span>';
     
     var initialOffset = window.globalStorage === undefined ? 0 : -17;
     
