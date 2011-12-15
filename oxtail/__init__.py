452c52f40b150011aee4ee26f59a10a424827457
Andrew Pendleton <abpend@gmail.com>
2011-12-15 12:37:01 +0200

    Version bump.

--- oxtail/__init__.py
+++ oxtail/__init__.py
@@ -1,4 +1,4 @@
-__extension_version__ = '0.4.2'
+__extension_version__ = '0.4.3'
 
 import subprocess, os
 git = subprocess.Popen('cd %s; git log -n 1 --raw --oneline' % os.path.dirname(os.path.abspath(__file__)), shell=True, stdout=subprocess.PIPE)
