75de2b618a4eae33e2c586719175f2282647fff4
Andrew Pendleton <abpend@gmail.com>
2011-12-15 11:34:19 +0200

    Fixing oxtail breakage again.

--- oxtail/templates/oxtail/button_adder.js
+++ oxtail/templates/oxtail/button_adder.js
@@ -1,7 +1,7 @@
 var submit = document.getElementById('oxtail-submit');
 if (!submit) {
-    var searchTable = document.getElementsByClassName('GcwpPb-imj1dd')[0];
+    var searchTable = document.querySelectorAll('.no .nn .oy8Mbf')[0];
     var userArea = document.createElement('td');
-    searchTable.childNodes[0].childNodes[0].appendChild(userArea);
+    searchTable.childNodes[0].appendChild(userArea);
     userArea.innerHTML = userArea.innerHTML + '<button id=\'oxtail-submit\' style=\'background: none repeat scroll 0% 0% transparent; border: 0; padding: 0; margin: 0; width: 166px; height: 34px; position: absolute; right: 20px; top: 40px; z-index: 999; cursor: pointer;\'><img id=\'oxtail-submit-img\' src=\'{{ host }}{{ oxtail_media_path }}/img/button-off.png\' style=\'position: absolute; left: 0px; clip: rect(0px,166px,34px,0px);\' /></button>';
  }
