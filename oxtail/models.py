83322a2afcef3e3c0f08091ca74317a2f253b192
Andrew Pendleton <apendleton@sunlightfoundation.com>
2012-03-13 18:09:24 -0400

    Switch array field libraries because the other one seemed not to work.

--- oxtail/models.py
+++ oxtail/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from oxtail.fields import UUIDField, TruncatingCharField
 from django.conf import settings
-import dbarray
+from arrayfields.fields import CharArrayField
 
 class Entity(models.Model):
     id = UUIDField(primary_key=True, auto=False, db_index=True)
     crp_id = models.CharField(max_length=16, db_index=True, blank=True)
-    aliases = dbarray.CharArrayField(max_length=255, default=[])
+    aliases = CharArrayField(max_length=255, default='')
     json = models.TextField()
 
 class Employer(models.Model):
