3c424f2cab9926e19d7ae3e862f14262213992d2
Andrew Pendleton <apendleton@sunlightfoundation.com>
2012-10-11 15:33:03 -0400

    Make the extension somewhat less broken.

--- oxtail/templates/oxtail/crx.js
+++ oxtail/templates/oxtail/crx.js
@@ -1,16 +1,12 @@
 var doc = document;
 var oxtailInterval = setInterval(function() {
-    var frame = doc.getElementById('canvas_frame');
-    if (frame && frame.contentDocument) {
-        var document = frame.contentDocument;
-        if (document.getElementsByClassName('w-as0').length) {
-            clearInterval(oxtailInterval);
-            {% include 'oxtail/button_adder.js' %}
-            
-            var loader = document.createElement('script');
-            loader.setAttribute('type', 'text/javascript');
-            loader.src = 'data:text/javascript;charset=utf-8,' + encodeURIComponent("{% filter escapejs %}{% include 'oxtail/loader.js' %}{% endfilter %}");
-            document.getElementsByTagName('head')[0].appendChild(loader);
-        }
+    if (document.getElementById('gbvg')) {
+        clearInterval(oxtailInterval);
+        {% include 'oxtail/button_adder.js' %}
+        
+        var loader = document.createElement('script');
+        loader.setAttribute('type', 'text/javascript');
+        loader.src = 'data:text/javascript;charset=utf-8,' + encodeURIComponent("{% filter escapejs %}{% include 'oxtail/loader.js' %}{% endfilter %}");
+        document.getElementsByTagName('head')[0].appendChild(loader);
     }
 }, 250);
